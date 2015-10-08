a5e5cb88e0d9e174343861dee4683edbd6b5a0d1
Ray Qiu <ray.qiu@gmail.com>
2015-10-07 22:53:31 -0400

    Updated readData.R

--- readData.R
+++ readData.R
@@ -1,25 +1,25 @@
 # Import required libraries
 library(lubridate)
+library(readr)
 
-# See if cached data exists, if not, download it
+# Check if cached data exists
 if (!file.exists("myCachedData.rds")) {
-    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
-                 method = "curl", destfile = "household_power_consumption.zip")
+    # Cached data doesn't exist, check whether zip data file exists
+    if (!file.exists("household_power_consumption.zip")) {
+      # Zip file does not exist, download from web
+      download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                   method = "curl", destfile = "household_power_consumption.zip")
+    }
     unzip("household_power_consumption.zip")
     # Read in data
-    h <- read.table('household_power_consumption.txt', header=TRUE,
-                    sep=';', na.strings='?',
-                    colClasses=c(rep('character', 2), rep('numeric', 7)))
-    
-    # Convert dates and time to R objects
-    h$Date <- dmy(h$Date)
-    h$Time <- hms(h$Time)
-    
-    # Remove rows with NA value, and keep only rows between 2007-02-01 
-    # and 2007-02-02.
-    dat <- subset(h, Date >= ymd("2007-02-01") & Date <= ymd("2007-02-02"))
-    # Create a single object to have both Date and Time
-    dat$Date.Time <- dat$Date + dat$Time
+    h <- read_delim("household_power_consumption.txt", ";", col_names = TRUE,
+                    na = '?', col_types = "ccnnnnnnn")
+    # Combine date and time to a single R object
+    h$Date.Time <- dmy_hms(paste(h$Date, h$Time))
+    # Keep only rows for 2007-02-01 and 2007-02-02.
+    dat <- h %>% 
+           filter(Date.Time >= ymd("2007-02-01") & Date.Time < ymd("2007-02-03"))
+    # Cache data
     saveRDS(dat, file = "myCachedData.rds")
 } else {
     dat <- readRDS("myCachedData.rds")
